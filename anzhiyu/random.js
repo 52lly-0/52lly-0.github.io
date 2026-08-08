75c8f572fb9303541a934bb70a1101c87d8b8e8d
52lly-0 <admin@wlyx666.eu.cc>
2026-08-08 10:50:54 +0800

    Site updated: 2026-08-08 10:50:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/08/第三方节点转换/"];function toRandomPost(){
+var posts=["2026/08/08/感谢博主视频详细讲解（上）/","2026/08/08/第三方节点转换/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

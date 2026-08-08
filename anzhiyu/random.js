e325615ca6d10cf9c41a483ff4e2ad7381453233
52lly-0 <admin@wlyx666.eu.cc>
2026-08-08 10:39:30 +0800

    Site updated: 2026-08-08 10:39:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/08/hello-world/"];function toRandomPost(){
+var posts=["2026/08/08/第三方节点转换/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
